db37d681b01319b45d6930f8a498ad566bbeb0ff
Budai Sándor <sbudai.ga@gmail.com>
2020-09-05 20:30:57 +0200

    package prefixes added (I was not able to test and try without them)

--- R/utils.R
+++ R/utils.R
@@ -1,6 +1,7 @@
 #' Create Eurostat Source Description
 #' 
-#' @importFrom eurostat label_eurostat_tables
+#' @importFrom eurostat label_eurostat_tables get_eurostat
+#' @importFrom magrittr %>%
 #' @keywords internal
  
 create_eurostat_source <- function(code) {
@@ -13,7 +14,7 @@ eurostat_download_label <- function (code) {
   tmp <- eurostat::get_eurostat(code, time_format = "num") 
   join_by_vars <- names(tmp)[! names(tmp) %in% c("time", "values", "geo")]
   
-  tmp %>% left_join ( eurostat_create_metadata_information(tmp), by = join_by_vars )
+  tmp %>% dplyr::left_join( eurostat_create_metadata_information(tmp), by = join_by_vars )
 }
 
 #' @importFrom dplyr select distinct_all left_join bind_cols
@@ -23,10 +24,10 @@ eurostat_download_label <- function (code) {
 eurostat_create_metadata_information <- function (dat) {
     
     unique_metadata <- dat %>%
-      select ( -all_of(c("values", "time", "geo")) ) %>%
-      distinct_all()
+      dplyr::select( -all_of( c( "values", "time", "geo" ) ) ) %>%
+      dplyr::distinct_all()
     
-    unique_metadata %>% bind_cols( 
+    unique_metadata %>% dplyr::bind_cols( 
       unique_metadata %>% 
         eurostat::label_eurostat() %>%
         purrr::set_names (paste0(names(.), "_label")))
